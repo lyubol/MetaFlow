33376aeefa65790e70f6ec2849df361bb0aaa37c
lyubol <lyubomir.lirkov@gmail.com>
2025-05-18 14:46:42 +0100

    Test

--- fabric_client.py
+++ fabric_client.py
@@ -60,9 +60,9 @@ class FabricClient:
         else:
             print(f"Error: {response.status_code} - {response.text}")
             return None
-
+        
     def test(self):
-        None
+        return None
 
 # Execution starts here
 if __name__ == "__main__":
